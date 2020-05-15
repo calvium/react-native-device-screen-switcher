e79499744c5755f3cae9ba58bb2286211abc0322
Konstantin Koulechov <kostek@sni.pl>
2020-05-15 13:09:04 +0200

    Add iPhone 11 sizes

--- src/deviceSizes.js
+++ src/deviceSizes.js
@@ -16,7 +16,7 @@ const deviceSizes = {
       fontScale: 2,
     },
   },
-  'iPhone 6': { // 6s, 7, 8
+  'iPhone 6, 6s, 7, 8)': { // 6s, 7, 8
     windowPhysicalPixels: {
       width: 750,
       height: 1334,
@@ -24,7 +24,7 @@ const deviceSizes = {
       fontScale: 2,
     },
   },
-  'iPhone 6 Plus': { // 6s+, 7+, 8+
+  'iPhone 6 Plus, 6s+, 7+, 8+': { // 6s+, 7+, 8+
     windowPhysicalPixels: {
       width: 1242,
       height: 2208,
@@ -32,10 +32,26 @@ const deviceSizes = {
       fontScale: 3,
     },
   },
-  'iPhone X': { // note: this won't have the bezel
+  'iPhone X, XS, 11 Pro': { // note: this won't have the bezel
     windowPhysicalPixels: {
        width: 1125,
        height: 2436,
+       scale: 3,
+       fontScale: 3,
+   },
+  },
+  '11, XR': {
+    windowPhysicalPixels: {
+       width: 828,
+       height: 1792,
+       scale: 2,
+       fontScale: 2,
+   },
+  },
+  '11 Pro Max, XS Max': {
+    windowPhysicalPixels: {
+       width: 1242,
+       height: 2688,
        scale: 3,
        fontScale: 3,
    },
